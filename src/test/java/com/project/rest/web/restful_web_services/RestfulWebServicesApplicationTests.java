6514c1685802138fb97a9281f432e03b9bf9580f
prabhatxmishra <prabhatmishra2402@gmail.com>
2026-03-25 00:21:15 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/project/rest/web/restful_web_services/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rest.web.restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
